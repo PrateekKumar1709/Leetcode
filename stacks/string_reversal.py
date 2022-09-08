37e9eb613ffcf153f8be3f6e4985824f2db487a0
PrateekKumar1709 <pk2460@nyu.edu>
2022-09-07 20:38:47 -0400

    Updated string reversal code

--- stacks/string_reversal.py
+++ stacks/string_reversal.py
@@ -1,5 +1,13 @@
 from stacks.stack import Stack
 
 def str_reversal (string):
+    rev_str=""
     for i in range(len(string)):
+        myStack.push(string[i])
+    for i in range(len(string)):
+        rev_str = rev_str + (myStack.pop())
+    print(rev_str)    
         
+myStack = Stack()
+str_reversal("String")
+
