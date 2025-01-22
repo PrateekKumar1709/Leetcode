bd6f721f605352a1cc29e46623f8ca4690346cea
Prateek Kumar <85689959+PrateekKumar1709@users.noreply.github.com>
2025-01-22 10:53:08 -0500

    Time: 30 ms (97.11%), Space: 37.1 MB (42.6%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.py
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.py
@@ -1,12 +1,9 @@
-from enum import Enum
 from typing import List
 
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
-        unique = sorted(set(arr))
-        Rank = Enum('Rank', {str(uni): i+1 for i, uni in enumerate(unique)})
-        for i in range(len(arr)):
-            arr[i] = Rank[str(arr[i])].value
+        # Create a dictionary mapping unique values to their ranks
+        rank_map = {val: rank for rank, val in enumerate(sorted(set(arr)), 1)}
         
-        return arr
-
+        # Use a list comprehension to create the result
+        return [rank_map[num] for num in arr]
