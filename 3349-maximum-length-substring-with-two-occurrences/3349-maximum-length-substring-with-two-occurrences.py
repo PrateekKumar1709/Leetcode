2d4422056d661087466996573240155a06132fda
Prateek Kumar <85689959+PrateekKumar1709@users.noreply.github.com>
2025-01-25 12:15:33 -0500

    Time: 3 ms (87.32%), Space: 17.7 MB (34.74%) - LeetHub

--- 3349-maximum-length-substring-with-two-occurrences/3349-maximum-length-substring-with-two-occurrences.py
+++ 3349-maximum-length-substring-with-two-occurrences/3349-maximum-length-substring-with-two-occurrences.py
@@ -7,13 +7,11 @@ class Solution:
             char = s[right]
             freq[char] = freq.get(char, 0) + 1
             
-            # If current character's count exceeds 2, move left pointer to the right
             while freq[char] > 2:
                 left_char = s[left]
                 freq[left_char] -= 1
                 left += 1
-            
-            # Update the maximum length
+
             current_length = right - left + 1
             if current_length > max_len:
                 max_len = current_length
