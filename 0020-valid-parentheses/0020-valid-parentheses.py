6c3580ff498872bf5286361956aef0e0310a8a7c
Prateek Kumar <85689959+PrateekKumar1709@users.noreply.github.com>
2025-01-24 22:21:00 -0500

    Time: 0 ms (100%), Space: 18 MB (7.01%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -7,7 +7,6 @@ class Solution:
         stack = []
 
         for i in range(0,len(s)):
-            
             if s[i] in [']',')','}'] and len(stack) != 0 :
                 if dict.get(s[i]) == stack[-1]:
                     stack.pop()
@@ -15,6 +14,7 @@ class Solution:
                     return False
             else:
                 stack.append(s[i])
+                
         if len(stack) == 0:
             return True
         else:
