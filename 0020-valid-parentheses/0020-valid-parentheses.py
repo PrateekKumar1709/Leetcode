afc8700db2db34953d598d1685e81bfe424f354d
Prateek Kumar <85689959+PrateekKumar1709@users.noreply.github.com>
2025-01-24 22:19:44 -0500

    Time: 0 ms (100%), Space: 18.2 MB (7.01%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 == 1:
+            return False
+        
+        dict = {']':'[','}':'{',')':'('}
+        stack = []
+
+        for i in range(0,len(s)):
+            
+            if s[i] in [']',')','}'] and len(stack) != 0 :
+                if dict.get(s[i]) == stack[-1]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(s[i])
+        if len(stack) == 0:
+            return True
+        else:
+            return False
