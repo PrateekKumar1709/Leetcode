0b0cf4218847c22ea93508502e8a94bac5cbe325
Prateek Kumar <85689959+PrateekKumar1709@users.noreply.github.com>
2025-02-07 08:28:14 -0500

    Time: 0 ms (100%), Space: 18.1 MB (34.68%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,9 +1,17 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        n = len(nums)
-        dp = [1] * n
-        for i in range(n):
-            for j in range(i):
-                if nums[i] > nums[j]:
-                    dp[i] = max(dp[i], dp[j] + 1)
-        return max(dp) if dp else 0
+        dp = []
+        
+        for num in nums:
+            # Find the position to insert the current number
+            i = bisect_left(dp, num)
+            
+            # If we should append to the end
+            if i == len(dp):
+                dp.append(num)
+            # Replace the number at position i
+            else:
+                dp[i] = num
+        
+        return len(dp)
+
