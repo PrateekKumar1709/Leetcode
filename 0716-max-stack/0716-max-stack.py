8ee24951cc8cbe3ac32ad797eb9c9b6244085003
Prateek Kumar <85689959+PrateekKumar1709@users.noreply.github.com>
2025-02-07 08:38:58 -0500

    Time: 198 ms (41.33%), Space: 68.7 MB (62%) - LeetHub

--- 0716-max-stack/0716-max-stack.py
+++ 0716-max-stack/0716-max-stack.py
@@ -1,72 +1,39 @@
-from sortedcontainers import SortedDict
-class Node:
-    def __init__(self, val=0):
-        self.val = val
-        self.prev = None
-        self.next = None
+import heapq
+
 
 class MaxStack:
+
     def __init__(self):
-        # Initialize head and tail sentinels
-        self.head = Node()
-        self.tail = Node()
-        self.head.next = self.tail
-        self.tail.prev = self.head
-        # TreeMap to store value to nodes mapping
-        self.value_to_nodes = SortedDict()
-        
+        self.heap = []
+        self.cnt = 0
+        self.stack = []
+        self.removed = set()
+
     def push(self, x: int) -> None:
-        # Create new node
-        node = Node(x)
-        # Insert before tail
-        node.prev = self.tail.prev
-        node.next = self.tail
-        self.tail.prev.next = node
-        self.tail.prev = node
-        # Add to TreeMap
-        if x not in self.value_to_nodes:
-            self.value_to_nodes[x] = []
-        self.value_to_nodes[x].append(node)
-        
+        heapq.heappush(self.heap, (-x, -self.cnt))
+        self.stack.append((x, self.cnt))
+        self.cnt += 1
+
     def pop(self) -> int:
-        if self.head.next == self.tail:
-            return None
-        # Get top node
-        node = self.tail.prev
-        val = node.val
-        # Remove from doubly linked list
-        self._remove_node(node)
-        # Remove from TreeMap
-        self.value_to_nodes[val].pop()
-        if not self.value_to_nodes[val]:
-            del self.value_to_nodes[val]
-        return val
-    
+        while self.stack and self.stack[-1][1] in self.removed:
+            self.stack.pop()
+        num, idx = self.stack.pop()
+        self.removed.add(idx)
+        return num
+
     def top(self) -> int:
-        if self.head.next == self.tail:
-            return None
-        return self.tail.prev.val
-    
+        while self.stack and self.stack[-1][1] in self.removed:
+            self.stack.pop()
+        return self.stack[-1][0]
+
     def peekMax(self) -> int:
-        if not self.value_to_nodes:
-            return None
-        return self.value_to_nodes.peekitem(-1)[0]
-    
+        while self.heap and -self.heap[0][1] in self.removed:
+            heapq.heappop(self.heap)
+        return -self.heap[0][0]
+
     def popMax(self) -> int:
-        if not self.value_to_nodes:
-            return None
-        # Get max value and its last node
-        max_val = self.value_to_nodes.peekitem(-1)[0]
-        node = self.value_to_nodes[max_val][-1]
-        # Remove node
-        self._remove_node(node)
-        # Update TreeMap
-        self.value_to_nodes[max_val].pop()
-        if not self.value_to_nodes[max_val]:
-            del self.value_to_nodes[max_val]
-        return max_val
-    
-    def _remove_node(self, node):
-        # Helper to remove node from doubly linked list
-        node.prev.next = node.next
-        node.next.prev = node.prev
+        while self.heap and -self.heap[0][1] in self.removed:
+            heapq.heappop(self.heap)
+        num, idx = heapq.heappop(self.heap)
+        self.removed.add(-idx)
+        return -num
