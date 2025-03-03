29597f63b170361ee9e80b15e79cedd040dc1faa
Prateek Kumar <85689959+PrateekKumar1709@users.noreply.github.com>
2025-03-03 08:46:08 -0500

    Time: 0 ms (100%), Space: 17.9 MB (32.32%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -1,5 +1,7 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
+        # Handle negative numbers by converting to unsigned
+        n = n & 0xFFFFFFFF if n < 0 else n
         count = 0
         while n:
             n &= (n - 1)
